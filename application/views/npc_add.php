b61818d26282e22cc18327d05d17309a0f87d18f
yuyuanhey <cook19960626@gmail.com>
2018-01-12 23:16:01 +0800

    front-end design for add page (monster) and login

---
+++ application/views/npc_add.php
@@ -0,0 +1,15 @@
+ <p class="title">Monster Add</p>
+<?php
+$attributes = array(
+	'id' => 'add',
+	'name' => 'add',
+	'method' => 'GET'
+);
+echo form_open('npc/add/',$attributes);
+?>
+Name:<input type="text" name="name" size="8">
+Description:<input type="text" name="description">
+ImgUrl:<input type="text" name="imgurl">
+ <input type="submit" value="add">
+
+ </form>
