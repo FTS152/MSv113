723659224221b5c9b5677daec0c06be0e293e3ae
Chen Yu Yuan <cook19960626@gmail.com>
2018-01-14 01:37:39 +0800

    Update npc_add.php

--- application/views/npc_add.php
+++ application/views/npc_add.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
  <form style="width:33.33%; margin-left:33.33%;margin-bottom: 50px">
 	<div class="form-group">
 		<label for="name">NPC名稱: </label>
@@ -14,20 +13,3 @@
 	</div>
 	<button class="submit" style="margin-left:37.5%">EDIT</button>
 </form>
-=======
- <p class="title">Monster Add</p>
-<?php
-$attributes = array(
-	'id' => 'add',
-	'name' => 'add',
-	'method' => 'GET'
-);
-echo form_open('npc/add/',$attributes);
-?>
-Name:<input type="text" name="name" size="8">
-Description:<input type="text" name="description">
-ImgUrl:<input type="text" name="imgurl">
- <input type="submit" value="add">
-
- </form>
->>>>>>> origin/master
